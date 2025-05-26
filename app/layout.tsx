c579c867353c833d6c833b7a16d3081a9930c3f4
Rodrigo Piccilli <rgpiccil@ncsu.edu>
2025-05-26 15:54:42 -0400

    Finished Authentication Set Up.

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Open_Sans } from "next/font/google";
 import "./globals.css";
 
+import {ClerkProvider} from '@clerk/nextjs'
+
 
 
 const font = Open_Sans({
@@ -19,12 +21,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${font.className} ${font.className} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
+    <ClerkProvider
+    afterSignOutUrl="/"
+    >
+        <html lang="en">
+        <body
+            className={`${font.className} ${font.className} antialiased`}
+        >
+            {children}
+        </body>
+        </html>
+    </ClerkProvider>
   );
 }
